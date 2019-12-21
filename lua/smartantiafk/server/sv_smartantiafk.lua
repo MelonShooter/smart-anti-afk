a3b8d8cb7bce247ba71d9b6898813b6983e93e87
MelonShooter <14815068+MelonShooter@users.noreply.github.com>
2019-12-21 00:02:08 -0800

    Added IsSmartAntiAFK() and AFKTime()

--- lua/smartantiafk/server/sv_smartantiafk.lua
+++ lua/smartantiafk/server/sv_smartantiafk.lua
@@ -1,12 +1,37 @@
 util.AddNetworkString("SendAFKMessage")
+
 local plymeta = FindMetaTable("Player")
+local antiAFKPlayers = {}
 
 --need a function to start AFK to pause UTime and mark as AFK
---need a function to check if AFK
---need function checking how long afk
 --need function to end AFK and mark as not AFK anymore
---need to mark as unAFK is disconnect
---need table of table of afk player's identity, time AFK (0 if not AFK)
+--need to mark as unAFK on disconnect
+--need table of player identity (steamID) as key and time AFK, remove key if not AFK
 --use table to kick, ghost, and/or stop salary of player
+--need a function to check if AFK (hecho)
+--need function checking how long afk (hecho)
+
+
 local function startSmartAntiAFK()
+
+end
+
+local function endSmartAntiAFK()
+
+end
+
+--[[
+Detects if player is AFK. Returns true if they are. Returns false if they are not.
+]]
+
+function plymeta:IsSmartAntiAFK()
+	return IsValid(antiAFKPlayers[self:SteamID()])
+end
+
+--[[
+Returns the amount of time a player has been AFK. Returns 0 if they are not AFK
+]]
+
+function plymeta:AFKTime()
+	return IsValid(antiAFKPlayers[self:SteamID()] and antiAFKPlayers[self:SteamID()] or 0
 end
